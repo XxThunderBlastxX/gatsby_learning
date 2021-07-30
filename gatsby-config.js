a75ebc43fc1f175078340c89e0b883037639c964
xXThunderBlastxX <koustavmondal55@gmail.com>
2021-07-31 02:51:35 +0530

    completed till 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "learn_gatsby",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+  "gatsby-plugin-gatsby-cloud",
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",],
 };
